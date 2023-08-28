c80a1a47a69ba1d5b3fb0a702d22abc752357346
rbzf <anna.sadkowska@gmail.com>
2023-08-28 15:47:16 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/as/bazafilmow/BazaFilmowApplication.java
@@ -0,0 +1,13 @@
+package pl.as.bazafilmow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaFilmowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazaFilmowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/as/bazafilmow/BazaFilmowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.as.bazafilmow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaFilmowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
